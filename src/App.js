b0af1849c0a580c7ba2c68300a887cda273a06b3
MarekBartczak <marek.bartczak@gmail.com>
2021-03-29 22:36:54 +0200

    o firmie

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Aux from "./hoc/aux";
 
 import NavBar from "./components/topSection/navbar/navbar";
 import Background from "./components/topSection/background/background";
-
+import ShortAboutSection from "./components/ShortAboutSection/shortAboutSection";
+import CallRequestForm from "./components/callRequestForm/callRequestForm";
 class App extends Component {
   render() {
     return (
       <Aux>
         <NavBar />
         <Background />
-        {/* <CallRequestForm /> */}
+        <CallRequestForm />
+        <ShortAboutSection />
         {/* <ShortAboutSection /> */}
         {/* <MainSection /> */}
         {/* <Footer /> */}
--- src/components/ShortAboutSection/shortAboutSection.js
+++ src/components/ShortAboutSection/shortAboutSection.js
@@ -9,16 +9,27 @@ class ShortAboutSection extends Component {
         <div className={styles.shortAboutSection}>
           <div className={styles.about}>
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam
-              minima voluptate dolorum repudiandae, commodi eaque sunt facilis
-              dolore. Id ex omnis, similique harum non animi! Beatae assumenda
-              modi fugiat! Quo similique minus tempore blanditiis, saepe ratione
-              assumenda maiores tenetur!
+              Oferowane domki drewniane wykonujemy z balika świerkowego lub
+              sosnowego (145/45 mm) w technologii węgłowej, tzn z wycięciem w
+              narożnikach, pozwalającym na zazębienie się kolejnych warstw ścian
+              tworząc zwartą i mocną konstrukcję. Używamy drewna suszonego
+              komorowo, o odpowiedniej wilgotności, impregnowanego. Podstawą w
+              naszej firmie jest jakość i dbałość o szczegóły, dlatego
+              zatrudniamy doświadczoną kadrę pracowniczą, a materiały
+              wykorzystywane do produkcji naszych domków są najwyższej jakości,
+              pozyskiwane z legalnych źródeł.
+              <br />
+              <br />
+              Jesteśmy producentem, dlatego możemy się dostosować do
+              indywidualnych preferencji klienta. Kompleksowo produkujemy i
+              montujemy, zarówno dla klientów indywidualnych i firm, jesteśmy w
+              stanie kompleksowo wyposażyć każdy ośrodek wczasowy. Wykonujemy
+              zamówienia na terenie całej Polski oraz za granicą
             </p>
           </div>
           <div className={styles.btns}>
             <button className={styles.ourProjects}>Nasze Projekty</button>
-            <button className={styles.aboutUs}>O Firmie</button>
+            {/* <button className={styles.aboutUs}>O Firmie</button> */}
           </div>
         </div>
       </Aux>
