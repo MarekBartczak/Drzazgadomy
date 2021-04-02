48ae42a192e23e971bcbeea2ce91fce023946fd2
MarekBartczak <marek.bartczak@gmail.com>
2021-04-02 14:16:51 +0200

    offer slide

--- src/components/footerSection/footer.js
+++ src/components/footerSection/footer.js
@@ -1,37 +1,40 @@
 import React, { Component } from "react";
 import styles from "./footer.module.css";
+import Aux from "../../hoc/aux";
 
 class Footer extends Component {
   render() {
     return (
-      <div className={styles.footer}>
-        <div className={styles.logo}></div>
-        <div className={styles.links}>
-          <div className={styles.about}>O Firmie</div>
-          <a
-            className={styles.carpentry}
-            href="http://www.zabudowyletniskowe.pl/"
-          >
-            {" "}
-            Stolarnia Gronek
-          </a>
-          <a
-            className={styles.carpentry}
-            href="http://pracowniapanafranciszka.pl/"
-          >
-            {" "}
-            Pracownia Pana Franciszka
-          </a>
-          <a
-            className={styles.carpentry}
-            href="http://konstrukcjedrewniane.eu/"
-          >
-            {" "}
-            Konstrukcje Drewniane
-          </a>
-          <div className={styles.contact}>Kontakt</div>
+      <Aux>
+        <div className={styles.footer}>
+          <div className={styles.logo}></div>
+          <div className={styles.links}>
+            <div className={styles.about}>O Firmie</div>
+            <a
+              className={styles.carpentry}
+              href="http://www.zabudowyletniskowe.pl/"
+            >
+              {" "}
+              Stolarnia Gronek
+            </a>
+            <a
+              className={styles.carpentry}
+              href="http://pracowniapanafranciszka.pl/"
+            >
+              {" "}
+              Pracownia Pana Franciszka
+            </a>
+            <a
+              className={styles.carpentry}
+              href="http://konstrukcjedrewniane.eu/"
+            >
+              {" "}
+              Konstrukcje Drewniane
+            </a>
+            <div className={styles.contact}>Kontakt</div>
+          </div>
         </div>
-      </div>
+      </Aux>
     );
   }
 }
